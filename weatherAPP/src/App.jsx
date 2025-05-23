573950a8306f50632d772befb9f05f09bd7a5e4e
udit <uditsingh9368@gmail.com>
2025-05-23 12:49:03 +0530

    Add files via upload

---
+++ weatherAPP/src/App.jsx
@@ -0,0 +1,34 @@
+import { useState } from 'react'
+import './App.css'
+
+function App(){
+  const [value , setValue]= useState(0);
+
+  return(
+       <>
+        <h1>Count {value} </h1>   
+        <button onClick={ 
+          () => {
+            setValue(value+1);
+
+          }
+        }>Add</button>
+
+        <br></br>
+         <br></br>
+
+        <button onClick={ 
+          () => {
+            setValue(value-1);
+            
+          }
+        }>Subtract</button>
+
+
+      </> 
+
+
+  )
+}
+
+export default App 
